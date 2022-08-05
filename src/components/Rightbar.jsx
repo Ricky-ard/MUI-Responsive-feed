15278a7e1af37ef622ebb3324fbde5708e149052
ricky ardiansyah <rickyrichk@gmail.com>
2022-08-05 23:09:14 +0700

    feed

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,15 +1,107 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const Rightbar = () => {
   return (
-    <Box
-      bgcolor="lightseagreen"
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Rightbar
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={8}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://source.unsplash.com/random/?human,man"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://source.unsplash.com/random/?human,girl"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://source.unsplash.com/random/?kid"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://source.unsplash.com/random/?human,old"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://source.unsplash.com/random/?human"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://source.unsplash.com/random/?human,animal"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://source.unsplash.com/random/?human,food"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://source.unsplash.com/random/?human,anime"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://source.unsplash.com/random/?human"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://source.unsplash.com/random/?human"
+          />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Posts
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src="https://source.unsplash.com/random/300×300/?camera"
+              alt="gambar"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://source.unsplash.com/random/300×300/?camera"
+              alt="gambar"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://source.unsplash.com/random/300×300/?camera"
+              alt="gambar"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://source.unsplash.com/random/300×300/?camera"
+              alt="gambar"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://source.unsplash.com/random/300×300/?camera"
+              alt="gambar"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://source.unsplash.com/random/300×300/?camera"
+              alt="gambar"
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Conversations
+        </Typography>
+      </Box>
     </Box>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,85 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import {
+  Article,
+  Home,
+  ModeNight,
+  People,
+  Person,
+  Settings,
+} from "@mui/icons-material";
 import React from "react";
 
 const Sidebar = () => {
   return (
-    <Box
-      bgcolor="purple"
-      flex={1}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#Group">
+              <ListItemIcon>
+                <People />
+              </ListItemIcon>
+              <ListItemText primary="Group" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#Person">
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Person" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#Page">
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary="Page" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#Setting">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#Setting">
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
